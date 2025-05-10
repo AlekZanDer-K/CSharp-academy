c268bc3125edf90c59b5c0608ef78f5ce58888b0
Пользователь <alex_fun999@tut.by>
2025-05-10 17:22:52 +0300

    add homework2

---
+++ HW2/HW2.1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace HW2._1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите ваше имя: ");
+        var userName = Console.ReadLine();
+
+        Console.WriteLine("Сколько вам полных лет: ");
+        var userAge = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Какой у вас рост (в метрах): ");
+        var userHeight = Convert.ToDouble(Console.ReadLine());
+
+        if (userName != "" && userAge > 0 && userHeight > 0)
+        {
+            Console.WriteLine($"\nПривет, {userName}!\n\tВаш возраст: {userAge} лет.\n\t\tВаш рост: {userHeight} м.");
+        }
+        else
+        {
+            Console.WriteLine("\nНекорректно введены данные!");
+        }
+
+        Console.ReadKey();
+    }
+}
---
+++ HW2/HW2.2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace HW2._2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Необходимо ввести два числа, для проведения основных математических операций между ними (+, -, *, /).\n");
+
+        Console.WriteLine("Введите перрвое число: ");
+        var numberOne = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число: ");
+        var numderTwo = Convert.ToDouble(Console.ReadLine());
+
+        if (numderTwo != 0)
+        {
+            Console.WriteLine($"\nРезультаты опраций над числами {numberOne} и {numderTwo}:\n" +
+                              $"Сумма:\t\t{numberOne + numderTwo}\nРазность:\t{numberOne - numderTwo}\n" +
+                              $"Произведение:\t{numberOne * numderTwo}\nДеление:\t{numberOne / numderTwo}");
+        }
+        else
+        {
+            Console.WriteLine("\nОшибка. Деление на 0!");
+        }
+        Console.ReadKey();
+    }
+}
