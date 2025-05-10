f7d42a9fdf3aece7f203b01f6d4187151a9c79b2
Пользователь <alex_fun999@tut.by>
2025-05-10 21:58:56 +0300

    Add homework3

---
+++ HW3/HW3.1/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace HW3._1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Для определения возрастной группы, введите полное количество лет:");
+        var userAgeStr = Console.ReadLine();
+        
+        if (userAgeStr == "")
+        {
+            Console.WriteLine("Возраст не был введён!");
+        }
+        else
+        {
+            var userAgeInt = Convert.ToInt32(userAgeStr);
+            if (userAgeInt < 0)
+            {
+                Console.WriteLine("Возраст не может быть отрицательным!");
+            }
+            else
+            {
+                Console.Write("Ваша возрастная группа: ");
+                if (userAgeInt >= 0 && userAgeInt <= 2)
+                {
+                    Console.WriteLine("младенец.");
+                }
+                else
+                {
+                    if (userAgeInt >= 3 && userAgeInt <= 12)
+                    {
+                        Console.WriteLine("ребёнок.");
+                    }
+                    else
+                    {
+                        if (userAgeInt >= 13 && userAgeInt <= 17)
+                        {
+                            Console.WriteLine("подросток.");
+                        }
+                        else
+                        {
+                            if (userAgeInt > 18 && userAgeInt <= 59)
+                            {
+                                Console.WriteLine("взрослый.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("пожилой.");
+                            }
+                        }
+                    }
+                }
+            }
+            if (userAgeInt % 2 == 0)
+            {
+                Console.WriteLine("Возраст чётный.");
+            }
+            else
+            {
+                Console.WriteLine("Возраст нечётный.");
+            }
+        }
+        Console.ReadKey();
+    }
+}
---
+++ HW3/HW3.2/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace HW3._2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите два булевых значения true или falase\n" +
+                          "для проведения логических операций между ними: & (И), | (ИЛИ), ^ (исключающее ИЛИ), ! (НЕ)\n");
+
+        Console.WriteLine("Введите первое значение p: ");
+        var booleanOneStr = Console.ReadLine();
+        Console.WriteLine("Введите второе значение q: ");
+        var booleanTwoStr = Console.ReadLine();
+
+        if (booleanOneStr == "" || booleanTwoStr == "")
+        {
+            Console.WriteLine("Значение не были введены частично или полностью!");
+        }
+        else
+        {
+            if (booleanOneStr == "true" || booleanOneStr == "false")
+            {
+                if (booleanTwoStr == "true" || booleanTwoStr == "false")
+                {
+                    var booleanOne = Convert.ToBoolean(booleanOneStr);
+                    var booleanTwo = Convert.ToBoolean(booleanTwoStr);
+
+                    Console.WriteLine($"_____________________________________________\n" +
+                                      $"p\tq\tp&q\tp|q\tp^q\t!p\n" +
+                                      $"_____________________________________________\n" +
+                                      $"{booleanOne}\t{booleanTwo}\t{booleanOne && booleanTwo}\t" +
+                                      $"{booleanOne || booleanTwo}\t{booleanOne ^ booleanTwo}\t{!booleanOne}\n" +
+                                      $"_____________________________________________");
+                }
+                else
+                {
+                    Console.WriteLine("Значения введены некорректно");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Значения введены некорректно");
+            }
+        }
+        Console.ReadKey();
+    }
+}
