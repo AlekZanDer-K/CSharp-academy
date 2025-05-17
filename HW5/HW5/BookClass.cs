0bc785f4172777649a096ba8f797ee567e5139df
Пользователь <alex_fun999@tut.by>
2025-05-17 14:17:08 +0300

    add homework5

---
+++ HW5/HW5/BookClass.cs
@@ -0,0 +1,88 @@
+﻿namespace HW5;
+
+internal class BookClass
+{
+    private string _title;
+    private string _author;
+    private int _pages;
+
+    public string Title
+    {
+        get
+        {
+            return _title;
+        }
+
+        set
+        {
+            if (String.IsNullOrEmpty(value) == true)
+            {
+                _title = "Unknown";
+            }
+            else
+            {
+                _title = value;
+            }
+        }
+    }
+
+    public string Author
+    {
+        get
+        {
+            return _author;
+        }
+
+        set
+        {
+            if (String.IsNullOrEmpty(value) == true)
+            {
+                _author = "Unknown";
+            }
+            else
+            {
+                _author = value;
+            }
+        }
+    }
+
+    public int Pages
+    {
+        get
+        {
+            return _pages;
+        }
+
+        set
+        {
+            if (value <= 0)
+            {
+                _pages = 0;
+            }
+            else
+            {
+                _pages = value;
+            }
+        }
+    }
+
+    public BookClass()
+    {
+        Title = "Unknown";
+        Author = "Unknown";
+        Pages = 0;
+    }
+
+    public BookClass(string title, string author, int pages)
+    {
+        Title = title;
+        Author = author;
+        Pages = pages;
+
+    }
+
+    public void GetDescription()
+    {
+        Console.WriteLine($"Название: {Title}, Автор: {Author}, Страниц: {Pages}\n");
+    }
+}
---
+++ HW5/HW5/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace HW5;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите название книги:");
+        var userTitle = Console.ReadLine();
+        Console.WriteLine("Введите атора книги:");
+        var userAuthor = Console.ReadLine();
+        Console.WriteLine("Введите количество страниц:");
+        var pages = Int32.TryParse(Console.ReadLine(), out var userPages);
+
+        if (pages == false)
+        {
+            userPages = 0;
+        }
+
+        var bookUser = new BookClass(userTitle, userAuthor, userPages);
+        var bookDefault = new BookClass();
+        var bookTest = new BookClass("Вечныый воитель", "Майкл Муркок", 509);
+
+        Console.WriteLine("\nИнформация о книге пользователя, перегруженный конструктор:");
+        bookUser.GetDescription();
+
+        Console.WriteLine("\nИнформация о книге, конструктор по умолчанию");
+        bookDefault.GetDescription();
+
+        Console.WriteLine("\nИнформация о книге, перегруженный конструктор:");
+        bookTest.GetDescription();
+    }
+}
