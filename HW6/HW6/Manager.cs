96258099ae39a1a292dfc83138d24a3ab23fec82
Пользователь <alex_fun999@tut.by>
2025-05-26 16:40:59 +0300

    add homework6

---
+++ HW6/HW6/Manager.cs
@@ -0,0 +1,46 @@
+﻿namespace HW6;
+
+public class Manager : EmployeeBase
+{
+    public double WorkExperience { get; set; }
+    public Project TargetProject { get; set; }
+
+    public Manager(string name, double workExperience) : base(name, "Менеджер")
+    {
+        WorkExperience = workExperience;
+    }
+
+    public override void GetDetails()
+    {
+        Console.WriteLine($"Имя: {Name}\nДолжность: {Position}\nСтаж: {WorkExperience}");
+        if (TargetProject != null)
+        {
+            if (string.IsNullOrEmpty(TargetProject.ProjectName) == false)
+            {
+                TargetProject.DisplayProjectInfo();
+            }
+            else Console.WriteLine("Проект не назначен.");
+        }
+        else
+        {
+            Console.WriteLine("Проект не назначен.");
+        }
+    }
+
+    public class Project
+    {
+        public string ProjectName { get; set; }
+        public DateTime Deadline { get; set; }
+
+        public Project(string projectName, DateTime deadLine)
+        {
+            ProjectName = projectName;
+            Deadline = deadLine;
+        }
+
+        public void DisplayProjectInfo()
+        {
+            Console.WriteLine($"Название проекта: {ProjectName}\nВремя сдачи проекта: {Deadline.ToLongDateString()}");
+        }
+    }
+}
